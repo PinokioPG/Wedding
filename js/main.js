cd236cd95b39c30126fc93b24dd7c4a4c1fda960
PinokioPG <101195341+PinokioPG@users.noreply.github.com>
2022-03-08 20:56:54 +0530

    added year

--- js/main.js
+++ js/main.js
@@ -266,23 +266,24 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("28 June 2021 20:00:00 GMT+05:30");			
-		endTime = (Date.parse(endTime) / 1000);
+		var startTime = new Date("28 June 2021 20:00:00 GMT+05:30");			
+		startTime = (Date.parse(startTime) / 1000);
 
 		var now = new Date();
 		now = (Date.parse(now) / 1000);
-
-		var timeLeft = endTime - now;
-
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+		var timeDone = now - startTime;
+		
+		var years = Math.floor(timeDone / (86400 * 365));
+		var days = Math.floor(timeDone / 86400); 
+		var hours = Math.floor((timeDone - (days * 86400)) / 3600);
+		var minutes = Math.floor((timeDone - (days * 86400) - (hours * 3600 )) / 60);
+		var seconds = Math.floor((timeDone - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
 		if (hours < "10") { hours = "0" + hours; }
 		if (minutes < "10") { minutes = "0" + minutes; }
 		if (seconds < "10") { seconds = "0" + seconds; }
 
+		$("#years").html(years + "<span>Years</span>");
 		$("#days").html(days + "<span>Days</span>");
 		$("#hours").html(hours + "<span>Hours</span>");
 		$("#minutes").html(minutes + "<span>Minutes</span>");
